ed30f72865c42d3b4e7390ed5f4ec4e33a0a0c0e
trongtangoc <lenhisomot@gmail.com>
2024-10-25 02:59:29 +1100

    update laptop

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ class Task {
 // Hàm hiển thị danh sách các nhiệm vụ ra giao diện
 function display(tasks) {
     let result = "<table>";
-    for ( let i = 0; i < tasks.length; i++ ) {
+    for (let i = 0; i < tasks.length; i++) {
         result += "<tr>";
         // result += `<td class="nameTask" id="taskName">${tasks[i].name}</td>`;
         let taskClass = "nameTask";
-        if ( tasks[i].completed ) {
+        if (tasks[i].completed) {
             taskClass += " completed";
             console.log(taskClass);
         } else {
@@ -24,7 +24,7 @@ function display(tasks) {
         result += `<td><button class="btn" onclick="editTask(${i})">Edit</button></td>`;
         result += `<td><button class="btn" onclick="deleteTask(${i})">Delete</button></td>`;
         result += `<td><button class="btn" onclick="toggleComplete(${i})">`;
-        if ( tasks[i].completed ) {
+        if (tasks[i].completed) {
             result += "Back";
         } else {
             result += "Complete";
@@ -37,6 +37,7 @@ function display(tasks) {
     document.getElementById("result").innerHTML = result;
 }
 
+
 function toggleComplete(index) {
     // thay đổi trạng thái hoàn thành của task
     tasks[index].completed = !tasks[index].completed;
@@ -47,23 +48,29 @@ function toggleComplete(index) {
 document.getElementById("add").addEventListener("click", function () {
     let taskName = document.getElementById("inputTaskValue").value;
     let task = {};
-    if ( taskName.length > 5 && taskName.length < 100 ) {
+    if (taskName.length > 5 && taskName.length < 100) {
         task = new Task(tasks.length + 1, taskName);
     } else {
         alert("Tên công việc phải từ 5 đến 100 ký tự");
         document.getElementById("taskInputValue").focus();
         return;
     }
+
+    document.getElementById("alertProcess").innerHTML = "Bạn đã thêm công việc thành công";
+
+
+    taskName.textContent = taskName
+    localStorage.setItem("inputTaskValue", taskName)
     tasks.push(task);
     display(tasks);
-    document.getElementById("alertProcess").innerHTML = "Bạn đã thêm công việc thành công";
+
     document.getElementById("inputTaskValue").value = "";
 });
 
 // Hàm xóa nhiệm vụ
 function deleteTask(index) {
     let isDelete = confirm(`Bạn có chắc muốn xóa công việc "${tasks[index].name}"?`);
-    if ( isDelete ) {
+    if (isDelete) {
         tasks.splice(index, 1);
         display(tasks);
         alert("Xóa thành công");
@@ -73,7 +80,7 @@ function deleteTask(index) {
 // Hàm sửa nhiệm vụ
 function editTask(index) {
     let newTaskName = prompt("Sửa công việc:", tasks[index].name);
-    if ( newTaskName !== null && newTaskName.trim() !== "" ) {
+    if (newTaskName !== null && newTaskName.trim() !== "") {
         tasks[index].name = newTaskName;
         display(tasks);
         alert("Cập nhật công việc thành công");
@@ -89,3 +96,15 @@ tasks.push(new Task(4, "Làm 10 bài tập trên Hackerank"));
 
 // Hiển thị danh sách nhiệm vụ lúc ban đầu
 display(tasks);
+
+// function displayUserName () {
+//     const nameFromLocalStorage = localStorage.getItem("name")
+//
+//     if (nameFromLocalStorage) {
+//         userNameText.textContent = nameFromLocalStorage
+//     } else {
+//         userNameText.textContent = "No name data in local storage"
+//     }
+// }
+//
+// displayUserName()
